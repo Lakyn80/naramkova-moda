d4c08cc569721057b593fdd8c31a3422dfea410a
Lakyn80 <lukas.krumpach@gmail.com>
2025-06-16 01:21:33 +0200

    dashobard_setting

--- backend/admin/routes.py
+++ backend/admin/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from . import admin_bp
 
 @admin_bp.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("admin/dashboard.html")
+    return render_template("admin/dashboard.html", user=current_user)
